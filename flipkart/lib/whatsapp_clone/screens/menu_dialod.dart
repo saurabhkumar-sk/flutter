ba42c97c6fef377c51b0bf2c9c274ce64b58395c
codecreatorskr <saurabhkumakairi501@gmail.com>
2023-08-29 02:35:58 +0530

    add dialog in menu button or avatar

--- flipkart/lib/whatsapp_clone/screens/menu_dialod.dart
+++ flipkart/lib/whatsapp_clone/screens/menu_dialod.dart
@@ -44,3 +44,46 @@ class MenuDialog extends StatelessWidget {
     ));
   }
 }
+
+
+
+// ListTile(
+//   onTap: () {
+//     showDialog(
+//       context: context,
+//       builder: (context) => FittedBox(
+//         fit: BoxFit.cover,
+//         child: Dialog(
+//           clipBehavior: Clip.hardEdge,
+//           child: Column(
+//             children: [
+//               Stack(
+//                 alignment: Alignment.topRight,
+//                 children: [
+//                   Image.asset(
+//                     chatList[index]['avatar'],
+//                     height: 40,
+//                     width: 50,
+//                   ),
+//                   IconButton(
+//                     icon: Icon(Icons.chat), // Add the chat icon
+//                     onPressed: () {
+//                       // Add your chat icon onTap logic here
+//                     },
+//                   ),
+//                   IconButton(
+//                     icon: Icon(Icons.info), // Add the info icon
+//                     onPressed: () {
+//                       // Add your info icon onTap logic here
+//                     },
+//                   ),
+//                 ],
+//               ),
+//               // Add other content to the dialog
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   },
+// )
