d589919e59afb5773efb89b4a10c42366321adf5
saurabhkumar-sk <saurabhkumakairi501@gmail.com>
2023-09-27 20:05:22 +0530

    Image picker =>> single image picker or multi image picker

--- flipkart/lib/Screens/imge_picker.dart
+++ flipkart/lib/Screens/imge_picker.dart
@@ -12,18 +12,46 @@ class ImagePickerScreen extends StatefulWidget {
 }
 
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
+  File? image;
+
+  List<File> imageList = [];
+
   void pick(ImageSource source) async {
     try {
       final picker = ImagePicker();
-      final pickedFile = await picker.pickImage(source: source);
+      final pickedFile = await picker.pickImage(
+        source: source,
+        preferredCameraDevice: CameraDevice.front,
+      );
       if (pickedFile != null) {
         log(pickedFile.path, name: 'pickedFile');
         final imageFile = File(pickedFile.path);
         log(imageFile.toString(), name: 'ImageFile');
+        setState(() {
+          image = imageFile;
+        });
+        // final base64 = base64Encode(await pickedFile.readAsBytes());
+        // log(base64, name: 'Base64');
       }
     } catch (e, s) {
       log(e.toString(), stackTrace: s, name: 'ImagePicker error');
     }
+  }
+
+  void pickMultiImage() async {
+    final picker = ImagePicker();
+    List<File> imageFiles = [];
+    final pickedFiles = await picker.pickMultiImage();
+    for (XFile element in pickedFiles) {
+      imageFiles.add(
+        File(element.path),
+      );
+    }
+    setState(() {
+      imageList.addAll(imageFiles);
+
+      // imageList = imageFiles;
+    });
   }
 
   @override
@@ -40,48 +68,84 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              pick(ImageSource.camera);
-              // pick(ImageSource.gallery);
-            },
-            child: Container(
-              margin: const EdgeInsets.only(
-                left: 100,
-                top: 15,
-              ),
-              height: 100,
-              width: 100,
-              color: const Color.fromARGB(255, 194, 229, 229),
-              child: const Icon(
-                Icons.add_a_photo,
-                color: Color.fromARGB(255, 1, 151, 197),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Wrap(
+              children: List.generate(
+                  imageList.length + 1,
+                  (index) => index == imageList.length
+                      ? GestureDetector(
+                          onTap: pickMultiImage,
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                              left: 10,
+                            ),
+                            height: 80,
+                            width: 80,
+                            color: Colors.cyan[100],
+                            child: const Icon(
+                              Icons.add_photo_alternate_outlined,
+                            ),
+                          ),
+                        )
+                      : Image.file(
+                          imageList[index],
+                          fit: BoxFit.cover,
+                          height: 80,
+                          width: 80,
+                        )),
+            ),
+            GestureDetector(
+              onTap: () {
+                pick(ImageSource.camera);
+                // pick(ImageSource.gallery);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 194, 229, 229),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                margin: const EdgeInsets.only(top: 50),
+                height: 100,
+                width: 100,
+                child: const Icon(
+                  Icons.add_a_photo,
+                  color: Color.fromARGB(255, 49, 121, 245),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 50),
-          GestureDetector(
-            onTap: () {
-              // pick(ImageSource.camera);
-              pick(ImageSource.gallery);
-            },
-            child: Container(
-              margin: const EdgeInsets.only(
-                left: 100,
-                top: 15,
-              ),
-              height: 200,
-              width: 200,
-              color: const Color.fromARGB(255, 194, 229, 229),
-              child: const Icon(
-                Icons.image,
-                color: Color.fromARGB(255, 1, 151, 197),
+            const SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                // pick(ImageSource.camera);
+                pick(ImageSource.gallery);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 194, 229, 229),
+                  image: image == null
+                      ? null
+                      : DecorationImage(
+                          image: FileImage(image!),
+                          fit: BoxFit.fill,
+                        ),
+                ),
+                height: 200,
+                width: 200,
+                child: image == null
+                    ? const Icon(
+                        Icons.image,
+                        color: Color.fromARGB(255, 1, 151, 197),
+                      )
+                    : null,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
