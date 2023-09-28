5862efded5009a8a03ec1bfe9113a4b4dd58b6b3
saurabhkumar-sk <saurabhkumakairi501@gmail.com>
2023-09-28 11:00:06 +0530

    Some advance widget like Transform, opacity, backdropfilter, cliprect,clipRrect

--- flipkart/lib/Screens/advance_widget.dart
+++ flipkart/lib/Screens/advance_widget.dart
@@ -1,4 +1,7 @@
 //Vertical Divider , IntrinsicHeight-Intrinsicwidth, ClipOval , ClipRect, ClipRRect, Opacity , Backdropfilter, Transform.
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AdvanceWidgetScrreen extends StatefulWidget {
@@ -9,6 +12,8 @@ class AdvanceWidgetScrreen extends StatefulWidget {
 }
 
 class _AdvanceWidgetScrreenState extends State<AdvanceWidgetScrreen> {
+  bool scale = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,38 +28,121 @@ class _AdvanceWidgetScrreenState extends State<AdvanceWidgetScrreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          const IntrinsicHeight(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'saurabh',
-                ),
-                VerticalDivider(
-                  thickness: 5,
-                ),
-                Text(
-                  'kumar',
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 15,
             ),
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          ClipOval(
-            child: Container(
-              height: 100,
-              width: 150,
-              color: Colors.cyan[100],
+            const IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'saurabh',
+                  ),
+                  VerticalDivider(
+                    thickness: 5,
+                  ),
+                  Text(
+                    'kumar',
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 25,
+            ),
+            ClipOval(
+              child: Container(
+                height: 100,
+                width: 150,
+                color: Colors.cyan[200],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+                topLeft: Radius.circular(50),
+              ),
+              child: Container(
+                height: 100,
+                width: 150,
+                color: Colors.cyan,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Opacity(
+              opacity: 1,
+              child: ClipRect(
+                child: Image.network('https://picsum.photos/250?image'),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // BackdropFilter(
+            //   filter: ImageFilter.blur(sigmaX: 1, sigmaY: 2),
+            //   child: Image.network('https://picsum.photos/250?image=1'),
+            // ),
+            // const SizedBox(
+            //   height: 15,
+            // ),
+            Transform.scale(
+              // scaleX: 2,
+              scale: scale ? 2 : 1,
+              child: SizedBox(
+                height: 100,
+                width: 100,
+                child: Image.network('https://picsum.photos/250?image=30'),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Transform.translate(
+              offset: scale ? const Offset(50, 100) : Offset.zero,
+              child: Image.network('https://picsum.photos/250?image=15'),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  scale = !scale;
+                });
+              },
+              child: const Icon(Icons.transform),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Transform.rotate(
+              angle: scale ? pi / 4 : 0,
+              child: Image.network('https://picsum.photos/250?image=25'),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Transform.flip(
+              flipX: scale ? true : false,
+              // flipY: true,
+              // origin: Offset(15, 20),
+              // filterQuality: FilterQuality.high,
+              // transformHitTests: true,
+              child: Image.network('https://picsum.photos/250?image=45'),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
